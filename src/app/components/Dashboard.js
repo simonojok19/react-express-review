3da9a706a0d642c6cf98fc4f7bd66bfc71fda48d
simonojok19 <simonojok19@gmail.com>
2021-09-27 20:08:50 +0300

    created the dashboard

---
+++ src/app/components/Dashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Dashboard = ({ groups }) => (
+  <div>
+    <h1>Dashboard</h1>
+  </div>
+);
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-console.log("Hello world")
+import React from "react";
+import ReactDom from "react-dom";
+import { Dashboard } from "./components/Dashboard";
+
+ReactDom.render(<Dashboard />, document.getElementById("app"));
